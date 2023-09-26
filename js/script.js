5e3cb157f9c642ab1f8f90a6acccbd15a13bc943
Eduardo Puerta <60192270+epuer18@users.noreply.github.com>
2023-09-26 19:47:13 -0400

    Add Alt Text to files

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 var first = true;
 
-function make_img(){
-    let stickfig = document.getElementById('stickfig')
-    for (let i = 0; i < 1; i+=0.05) {
-        setTimeout(() => {stickfig.style.opacity = i; }, 250)
-        console.log(i)
-      }
- 
+function make_img() {
+  let stickfig = document.getElementById("stickfig");
+  for (let i = 0; i < 1; i += 0.05) {
+    setTimeout(() => {
+      stickfig.style.opacity = i;
+    }, 250);
+    console.log(i);
+  }
 }
 
 window.onscroll = () => {
-    if((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight){
-        if(first){
-            first = false;
-            make_img();
-        }
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    if (first) {
+      first = false;
+      make_img();
     }
-}
+  }
+};
